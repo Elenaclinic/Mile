f7e18501e1d4fa01dde3f034b29d3f91edf9e661
Elena Isaeva <isayeva.yelena.94@mail.ru>
2022-11-03 20:59:34 +0300

    first commit

---
+++ src/Mile.java
@@ -0,0 +1,13 @@
+ public class Mile {
+    public static void main(String[] args) {
+
+
+        int money = 20; // количествo рублей для одной бонусной мили
+        int ticket = 100; // стоимость билета
+
+        System.out.println("Количество начисленных миль за купленный билет");
+        System.out.println(ticket / money);
+
+
+    }
+}
